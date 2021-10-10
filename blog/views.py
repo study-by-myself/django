7d3e2fde8ae1c3205f1db28b491ce1f5ab0d5449
genie <luvbliny@gmail.com>
2021-10-10 23:23:59 +0900

    feat post crud

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     """ blog list 출력 """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'blog/question_list.html', context)
 
 def detail(request, question_id):
@@ -21,6 +33,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'blog/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     blog 답변등록
@@ -30,6 +43,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -39,6 +53,7 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'blog/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     """
     blog 질문등록
@@ -47,6 +62,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('blog:index')
@@ -54,3 +70,37 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'blog/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    blog 질문수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('blog:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('blog:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'blog/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    blog 질문삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('blog:detail', question_id=question.id)
+    question.delete()
+    return redirect('blog:index')
